693978c321a998351389e850cc306efb6af9152f
isaacamaral <isaacalves29@gmail.com>
2021-08-03 11:16:32 -0300

    Adição do código para fechar o popup ao clicar fora dele em qualquer local da página

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ function abrirPopup() {
 function fecharPopup() {
     document.querySelector(".popup").style.display = 'none';
 }
+
+
+// Código para fechar o popup ao clicar fora dele em qualquer local da página
+$(document).mouseup(function(e) 
+{
+    var container = $(".popup");
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        container.hide();
+    }
+});
